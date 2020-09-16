9c506052ddf7565878b106e93832b2753d021757
mhuggi <emil.e.salmi@gmail.com>
2020-09-16 15:51:39 +0300

    init suggestions

--- phoneapp/src/App.vue
+++ phoneapp/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Suggestions />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Suggestions from './components/Suggestions'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Suggestions
   }
 }
 </script>
---
+++ phoneapp/src/components/Suggestions.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+        <h1>Suggestions</h1>
+
+    <div class="suggestions">
+        <div id="suggestion">
+            <h3>Helsinki senate square</h3>
+            <img class="pic" src="../assets/senate.jpg" alt="senatesquare" width="300">
+            <p class="info">The Senate Square presents Carl Ludvig Engel's architecture as a unique allegory of political, religious, scientific and commercial powers in the centre of Helsinki, Finland.</p>
+
+
+        </div>
+
+    </div>
+</div>
+</template>
+
+<style scoped>
+.suggestion {
+    display: grid;
+    grid-template-columns: 350px 600px;
+    grid-template-rows: 350px 350px;
+    grid-auto-flow: column;
+    justify-items: start;
+}
+.pic {
+    grid-column: 1;
+    grid-row: 1;
+}
+.info {
+    grid-column: 2;
+    grid-row: 2;
+}
+</style>
