7601d29377e8faa36cab89b7183bca285600271b
korhonel <70575523+korhonel@users.noreply.github.com>
2020-09-18 09:03:18 +0300

    Routes & RoutesFound tillagda

---
+++ phoneapp/src/components/RoutesFound.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container">
+        <h1>Choose suitable a route</h1>
+        <div id="itineraryMap">
+            <img src="../assets/ASLRoute.PNG" alt="ItineraryMap" id="ASLMap">
+        </div>
+        <a href="findRoute.html" >Back</a>  
+    </div>
+</template>
