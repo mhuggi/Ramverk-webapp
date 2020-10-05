c0755dbd4dc0e9d6aae628de7fb2b1d6dd25cfee
callealbrecht <33825129+callealbrecht@users.noreply.github.com>
2020-10-05 09:42:23 +0300

    Add files via upload
    Profile navigation + Reward page

---
+++ phoneapp/src/components/Navigation.vue
@@ -0,0 +1,95 @@
+<template>
+<div>
+    <div class="upper">
+    <h2> Your profile </h2>
+    
+    </div>
+
+<div class="navigation">
+   
+    <div class="useravatar">
+
+    </div>
+           
+    <p> John Smith </p> 
+    <p> Location </p>   
+</div>
+
+</div>
+
+<div class="pbutton">
+      <button class="button" type="button">Edit Profile</button>
+</div>
+
+ 
+
+
+</template>
+
+<style scoped>
+
+
+.navigation
+{
+    border: #bd572d solid 5px;
+    width: 50%;
+    height:500px;
+    text-align: center;
+    margin-left: 30%;
+    
+    
+
+
+}
+
+
+
+button {
+    background-color: #bd572d;
+    border: #bd572d solid 0px;
+    color: white;
+    width: 150px;
+    height: 50px;
+    padding: 10px 10px;
+    border-radius: 8px;
+    margin: 4px 4px;
+  
+  }
+  button:hover {
+    background-color: hsla(18, 62%, 46%, 0.898);
+  }
+  .button {
+  margin-top: 50px;
+  margin-left: 50%;
+  padding: 5px;
+  border-radius: 5px;
+
+ 
+    
+}
+.useravatar {
+    width: 100px;
+    height: 100px;
+    -moz-border-radius: 50px;
+    -webkit-border-radius: 50px;
+    border-radius: 60px;
+    border: #bd572d solid 5px;
+    margin-left:38%;
+    margin-top:100px;
+    
+   
+    
+}
+
+
+.upper {
+    margin-left: 50%;
+    margin-top:60px;
+  
+
+
+}
+
+
+
+  </style>
---
+++ phoneapp/src/components/NavigationReward.vue
@@ -0,0 +1,69 @@
+<template>
+
+<body>
+    <h2>Your Rewards</h2> 
+    <div class="piechart"></div> 
+
+</body> 
+
+
+      <button class="button" type="button">Claim Rewards</button>
+
+
+
+
+</template>
+
+
+<style> 
+
+
+
+
+
+	.piechart { 
+		margin-top: 300px; 
+		display: block; 
+		position: absolute; 
+		width: 400px; 
+		height: 400px; 
+        border: #bd2d58 solid 6px;
+		border-radius: 50%; 
+		background-image: conic-gradient(  
+			blue 0 260deg, 
+			white 0); 
+
+	} 
+
+	body, 
+	.piechart { 
+		display: flex; 
+		justify-content: center; 
+		align-items: center; 
+	} 
+
+
+button {
+    background-color: #bd572d;
+    border: #bd572d solid 0px;
+    color: white;
+    width: 150px;
+    height: 50px;
+    padding: 10px 10px;
+    border-radius: 8px;
+    margin: 4px 4px;
+  
+  }
+  button:hover {
+    background-color: hsla(18, 62%, 46%, 0.898);
+  }
+  .button {
+  margin-top: 500px;
+  margin-left: 0%;
+  padding: 5px;
+  border-radius: 5px;
+
+ 
+    
+}
+</style> 
