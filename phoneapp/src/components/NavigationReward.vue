99d34b7a478f17c6a7802b9b796f6fe3574f6cdf
callealbrecht <33825129+callealbrecht@users.noreply.github.com>
2020-10-07 09:52:22 +0300

    Last fixes to navigationRewards

--- phoneapp/src/components/NavigationReward.vue
+++ phoneapp/src/components/NavigationReward.vue
@@ -1,69 +1,50 @@
 <template>
+<div>
+  <body>
+    <h2>Your Rewards</h2>
+    <div class="piechart"></div>
+  </body>
 
-<body>
-    <h2>Your Rewards</h2> 
-    <div class="piechart"></div> 
-
-</body> 
-
-
-      <button class="button" type="button">Claim Rewards</button>
-
-
-
-
+  <button class="button" type="button">Claim Rewards</button>
 </template>
 
 
-<style> 
+<style>
+.piechart {
+  margin-top: 300px;
+  display: block;
+  position: absolute;
+  width: 400px;
+  height: 400px;
+  border: #bd2d58 solid 6px;
+  border-radius: 50%;
+  background-image: conic-gradient(blue 0 260deg, white 0);
+}
 
-
-
-
-
-	.piechart { 
-		margin-top: 300px; 
-		display: block; 
-		position: absolute; 
-		width: 400px; 
-		height: 400px; 
-        border: #bd2d58 solid 6px;
-		border-radius: 50%; 
-		background-image: conic-gradient(  
-			blue 0 260deg, 
-			white 0); 
-
-	} 
-
-	body, 
-	.piechart { 
-		display: flex; 
-		justify-content: center; 
-		align-items: center; 
-	} 
-
+body,
+.piechart {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
 button {
-    background-color: #bd572d;
-    border: #bd572d solid 0px;
-    color: white;
-    width: 150px;
-    height: 50px;
-    padding: 10px 10px;
-    border-radius: 8px;
-    margin: 4px 4px;
-  
-  }
-  button:hover {
-    background-color: hsla(18, 62%, 46%, 0.898);
-  }
-  .button {
+  background-color: #bd572d;
+  border: #bd572d solid 0px;
+  color: white;
+  width: 150px;
+  height: 50px;
+  padding: 10px 10px;
+  border-radius: 8px;
+  margin: 4px 4px;
+}
+button:hover {
+  background-color: hsla(18, 62%, 46%, 0.898);
+}
+.button {
   margin-top: 500px;
   margin-left: 0%;
   padding: 5px;
   border-radius: 5px;
-
- 
-    
 }
 </style> 
